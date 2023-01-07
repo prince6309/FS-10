3c4d0f76848fd7dc1d7fbf840c3970a17ff41435
Aman Srivastava <aman4863@gmail.com>
2023-01-07 21:47:03 +0530

    added

--- lec004/01. addOne/Solution.java
+++ lec004/01. addOne/Solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Solution {
     public static void printArr(int [] arr){
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
     }
