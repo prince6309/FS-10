78aba528f459de9607f9746b6e8dcca160aa35c1
Aman Srivastava <aman4863@gmail.com>
2023-01-10 20:43:42 +0530

    added

---
+++ lec006/01. slidingWindow/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        int n  = scn.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+        int k = scn.nextInt();
+        
+        //logic
+        for(int i = 0; i <= n-k; i++){
+            int maxVal = arr[i];
+            for(int j = 1; j<k; j++){
+                if(arr[i+j] > maxVal){
+                    maxVal = arr[i+j];
+                }
+            }
+            System.out.print(maxVal+" ");
+        }
+        
+    }
+}
