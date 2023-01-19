04eb6791499a966e49a6973d6e3d6017635f75d7
Aman Srivastava <aman4863@gmail.com>
2023-01-19 22:11:36 +0530

    added

---
+++ lec013/03. reverseArr/SolutionHW.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void reverse( ?write here?){
+        //write here
+    }
+
+    public static void main(String[] args) {
+       Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        reverse(?write here?);
+        for( i = 0; i < n; i++){
+          System.out.println(arr[i]);  
+        }
+        
+    }
+}
